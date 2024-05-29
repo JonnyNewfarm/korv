56e7bbb0e65abbc9ecb0cad2c9afd79d00982b32
Jonas Nygaard <jonasnygaard96@gmail.com>
2024-05-29 23:30:48 +0100

    .

--- korv/src/App.jsx
+++ korv/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useEffect, useState } from 'react'
+
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [product, setProduct] = useState([])
+
+  useEffect(()=> {
+    axios.get('http://localhost:3000/products')
+    .then(res => {setProduct(res.data.products)})
+    .catch( err => console.log(err))
+   
+
+  }, [])
+
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{color: "black"}}>
+
+{product.map((p, i ) => {
+  <ul key={i}>
+    <li>{p._id}</li>
+    <li>{p.name}</li>
+    <li>{p.price}</li>
+  </ul>
+})}
+
+
+    </div>
+  
     </>
   )
 }
